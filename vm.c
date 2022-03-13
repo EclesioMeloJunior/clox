4cfa87b059ff7777f61ec8287a07a5df4d246b9f
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2022-03-13 11:38:22 -0400

    chore: building the scanner, reading single tokens yet

--- vm.c
+++ vm.c
@@ -3,6 +3,7 @@
 #include "vm.h"
 #include "debug.h"
 #include "value.h"
+#include "compiler.h"
 
 VM vm;
 
@@ -71,9 +72,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRETER_OK;
 }
